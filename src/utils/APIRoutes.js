6ffbb6a1d38ed0988acb3e19dc45d6a2ecc980a5
Ritesh <riteshpatel246@gmail.com>
2023-12-26 01:52:31 +0530

    route

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = REACT_APP_URL;
+export const host = {REACT_APP_URL};
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
