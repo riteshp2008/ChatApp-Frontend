a47b6735495fcef9b445a1fd9082d01536d999ec
Ritesh <riteshpatel246@gmail.com>
2023-12-26 14:05:53 +0530

    url change

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import SetAvatar from './pages/SetAvatar'
 import Chat from './pages/Chat'
+import Protected from './ProtectedRoutes'
 
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  
+  const storedUser = sessionStorage.getItem("chat-app-user");
+
+  React.useEffect(() => {
+    if (storedUser) {
+      setIsLoggedIn(true);
+    }
+  }
+  , [storedUser]);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/setAvatar" element={<SetAvatar/>} />
-        <Route path="/chat" element={<Chat/>} />
+       
+            <>
+              <Route path="/" element={isLoggedIn ? <Chat /> : <Login setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/setAvatar" element={<Protected isLoggedIn={isLoggedIn}><SetAvatar /></Protected>} />
+              <Route path="/chat" element={<Protected isLoggedIn={isLoggedIn}><Chat /></Protected>} />
+              <Route path="/register" element={<Register />} />
+            </>
       </Routes>
+       
     </BrowserRouter>
   )
 }
